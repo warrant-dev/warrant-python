ad79cb089eade131d4d7ebad03628a1ad94b654e
stanleyphu <phu.stanley@gmail.com>
2022-12-13 12:26:32 -0800

    v0.3.0: Add query warrants method

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ README = (DIR / "README.md").read_text()
 
 setup(
     name="warrant-python",
-    version="0.2.2",
+    version="0.3.0",
     description="Python SDK for Warrant",
     long_description=README,
     long_description_content_type="text/markdown",
