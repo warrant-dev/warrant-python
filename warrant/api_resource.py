d2f89110f0a78b3cd6659caafd3286ccdf3a44dd
stanleyphu <phu.stanley@gmail.com>
2023-03-21 20:48:56 -0700

    Omit Authorization header when no api key is provided

--- warrant/api_resource.py
+++ warrant/api_resource.py
@@ -16,9 +16,10 @@ class APIResource(object):
     @classmethod
     def _get(cls, uri, params={}, object_hook=None):
         headers = {
-            "Authorization": "ApiKey " + warrant.api_key,
             "User-Agent": warrant.user_agent
         }
+        if warrant.api_key != "":
+            headers["Authorization"] = "ApiKey " + warrant.api_key
         resp = requests.get(url=warrant.api_endpoint+uri, headers=headers, params=params)
         if resp.status_code == 200:
             return resp.json(object_hook=object_hook)
@@ -28,9 +29,10 @@ class APIResource(object):
     @classmethod
     def _post(cls, uri, json={}, object_hook=None):
         headers = {
-            "Authorization": "ApiKey " + warrant.api_key,
             "User-Agent": warrant.user_agent
         }
+        if warrant.api_key != "":
+            headers["Authorization"] = "ApiKey " + warrant.api_key
         resp = requests.post(url=warrant.api_endpoint+uri, headers=headers, json=json)
         if resp.status_code == 200:
             return resp.json(object_hook=object_hook)
@@ -40,9 +42,10 @@ class APIResource(object):
     @classmethod
     def _put(cls, uri, json={}, object_hook=None):
         headers = {
-            "Authorization": "ApiKey " + warrant.api_key,
             "User-Agent": warrant.user_agent
         }
+        if warrant.api_key != "":
+            headers["Authorization"] = "ApiKey " + warrant.api_key
         resp = requests.put(url=warrant.api_endpoint+uri, headers=headers, json=json)
         if resp.status_code == 200:
             resp.json(object_hook=object_hook)
@@ -52,9 +55,10 @@ class APIResource(object):
     @classmethod
     def _delete(cls, uri, params={}, json={}):
         headers = {
-            "Authorization": "ApiKey " + warrant.api_key,
             "User-Agent": warrant.user_agent
         }
+        if warrant.api_key != "":
+            headers["Authorization"] = "ApiKey " + warrant.api_key
         resp = requests.delete(url=warrant.api_endpoint+uri, headers=headers, params=params, json=json)
         if resp.status_code != 200:
             raise WarrantException(msg=resp.text, status_code=resp.status_code)
